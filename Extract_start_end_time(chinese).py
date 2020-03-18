cc0419603b7a4afad318173a8045f3b3a974beeb
Zhean Shao <12205777+lanzero@users.noreply.github.com>
2020-03-18 01:49:09 -0700

    Modified for extraction of market data

--- Extract_start_end_time(chinese).py
+++ Extract_start_end_time(chinese).py
@@ -5,6 +5,8 @@ import collections
 
 
 a = collections.defaultdict(list)
+code = collections.defaultdict(str)
+
 with open("train_data.csv",'r', encoding="utf-8") as input:
     reader = csv.DictReader(input)
     for row in reader:
@@ -27,17 +29,21 @@ with open("train_data.csv",'r', encoding="utf-8") as input:
             else:
                 temp += char
         a[row["公司"]].append(y+'-'+m+'-'+d)
-print(a["盛运环保"])
+        code[row["公司"]] = row["代码"]
+# print(a["盛运环保"])
 for name in a.keys():
     a[name] = dict(collections.Counter(a[name]))
 print(a["盛运环保"])
-with open("ChineseCompany_1.csv",'w',encoding='utf-8') as out:
-    f = ['company','time and fequency']
-    out.write("{},{}\n".format(f[0],f[1]))
+with open("ChineseCompany_2.csv",'w',encoding='utf-8') as out:
+    f = ['code','company','start','end','count of date points']
+    out.write("{},{},{},{},{}\n".format(f[0],f[1],f[2],f[3],f[4]))
     for name in a.keys():
         temp = []
         for k,v in a[name].items():
-            if v >= 2:
+            if v > 1:
+            #the threshold is now 2, which means news count large than or equal to 2 will be counted as a date point
                 temp.append(tuple([k,v]))
         sorted(temp,key=lambda x: x[0])
-        out.write("{},{}\n".format(name, temp))
+        if temp:
+            if len(temp) >= 2:
+                out.write("{},{},{},{},{}\n".format(code[name],name,temp[0][0],temp[-1][0],len(temp)))
