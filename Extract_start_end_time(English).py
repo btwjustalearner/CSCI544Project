6932b1d2b9f9e7ae2194b50e74233dfcd337438f
lanzero <zheanshao@gmail.com>
2020-04-13 21:58:16 -0700

    english news

--- Extract_start_end_time(English).py
+++ Extract_start_end_time(English).py
@@ -2,8 +2,10 @@ import json
 import os
 import time
 from pathlib import Path
+import csv
 path = Path.cwd() / "us-financial-news-articles"
-data = {"google":{},"amazon":{},"apple":{}}
+data = {"google":{},"amazon":{},"apple":{},"microsoft":{},"facebook":{},"tesla":{},"intel":{}}
+for_write = []
 start = time.time()
 for root, dirs, files in os.walk(path):
     print(root)
@@ -20,14 +22,17 @@ for root, dirs, files in os.walk(path):
                             data[company][date].append(news_data['text'])
                         else:
                             data[company][date] = [news_data['text']]
+                        s = [company ,date ,news_data['title'].replace("\n"," ") ,news_data['text'].replace("\n"," ")]
+                        for_write.append(s)
     print('end\n')
 end = time.time()
 print("total:{}".format(end - start))
 
 print("-------- start stat -------\n")
 temp = []
+t = 0
 for company in data.keys():
-    print("Company: {}".format(company))
+    total = 0
     for date in data[company]:
         # print("{}, count_of_news:{}\n".format(date, len(data[company][date])))
         temp.append(tuple([date, len(data[company][date])]))
@@ -36,7 +41,17 @@ for company in data.keys():
         o.write("date, count\n")
         for item in temp:
             o.write("{},{}\n".format(item[0], item[1]))
+            total += item[1]
+    print("Company: {}, {}".format(company, total))
     temp = []
+    t += total
+print(t)
 print("---------start writing------\n")
 with open("news.json",'w') as t:
     json.dump(data, t)
+with open("news(en).csv",'w', encoding="utf-8") as f:
+    f.write("company, date, title, text\n")
+    for line in for_write:
+        f.write("{},{},{},{}\n".format(line[0],line[1],line[2],line[3]))
+
+
